5a6450f9fc2e89aea5b6ac1688806fe65623935f
Shipra <drshipradyundi@gmail.com>
2020-02-06 13:14:05 -0800

    adding models

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var exphbs = require("express-handlebars");
 var axios = require("axios");
 var cheerio = require("cheerio");
 // Require all models
-//var db = require("./models");
+var db = require("./models");
 
 var PORT = 3000
     // Initialize Express
